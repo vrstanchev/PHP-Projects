e69c50582658f4e3eda119bec755de1b11764030
vrstanchev <vrstanchev@tu-plovdiv.bg>
2023-05-04 16:20:26 +0300

    latest

--- RVSLatest/qr.py
+++ RVSLatest/qr.py
@@ -1,6 +1,6 @@
 import cv2
 import db
-cap = cv2.imread("tst.png")
+cap = cv2.imread("test1.png")
 detector = cv2.QRCodeDetector()
 data,b,c= detector.detectAndDecode(cap)
 print("[+] QR Code detected, data:", data)
