aaaca36a39e0d89b912268d69d272313488b8f7b
vrstanchev <vrstanchev@tu-plovdiv.bg>
2023-05-04 16:43:29 +0300

    latest1

--- RVSLatest/regadm.php
+++ RVSLatest/regadm.php
@@ -1,11 +1,10 @@
-<<<<<<< HEAD
+
 <!DOCTYPE HTML>
 <html>
     <head>
         <title>Add user</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" type="text/css" href="styles.css" />
     </head>
     <body>
         <h1>Регистрация</h1>
@@ -16,22 +15,3 @@
         </form>
     </body>
 </html>
-=======
-<!DOCTYPE HTML>
-<html>
-    <head>
-        <title>Add user</title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" type="text/css" href="styles.css" />
-    </head>
-    <body>
-        <h1>Регистрация</h1>
-        <form method="post" action="admadd.php" enctype="multipart/form-data">
-            <input type="text" name="uname1"  placeholder="Enter username:"></br>
-                <input type="password" name="upass"  placeholder="Enter password:"></br>
-  <input type="submit" value="Add" name="submit">
-        </form>
-    </body>
-</html>
->>>>>>> 35f2e9982123e32909c60864cbc51a18ca52f7f6
