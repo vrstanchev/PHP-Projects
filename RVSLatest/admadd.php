e181437e96642ceb2c790c183f78574252ffc2ee
Veselin Stanchev,BSc <vesodeveloper@gmail.com>
2023-05-04 20:34:30 +0300

    Update admadd.php

--- RVSLatest/admadd.php
+++ RVSLatest/admadd.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <?php 
 if($_POST){
   $FNAME=$_POST['uname1'] ;
@@ -29,35 +29,3 @@ if($_POST){
     }    
 }    
 ?>
-=======
-<?php 
-if($_POST){
-  $FNAME=$_POST['uname1'] ;
-    $LNAME=$_POST['upass'] ;
-     $encpass=md5($_POST['upass']);
-    try{
-        
-        include('conn.php'); 
-        
-     
-        
-        $stmt = $db -> prepare("INSERT INTO internal (UNAME,UPASS) VALUES (:UNAME,:UPASS);");
-       
-     
-        $stmt -> bindParam(':UNAME', $FNAME, PDO::PARAM_STR);
-         $stmt -> bindParam(':UPASS', $encpass, PDO::PARAM_STR);
-        
-       
-        if( $stmt -> execute() ){
-           header("Location:index.php");
-        }
-        
-        
-        $db = null;
-    }
-    catch (PDOExecption $e){
-        echo $e->getMessage();
-    }    
-}    
-?>
->>>>>>> 35f2e9982123e32909c60864cbc51a18ca52f7f6
